22a76f0726a6ba67cc2d234bf6ef2c3d9b4bd979
Gaspar Morales <gasparmorales1@gmail.com>
2023-11-02 15:58:23 -0300

    arreglo en el print

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 void print_bitmap(unsigned char *bitmap, size_t bitmap_size) {
     for (int i = 0; i < bitmap_size; i++) {
-        for (int j = 7; j >= 0; j--) {
+        for (int j = 0; j < 8; j++) {
             if (bitmap[i] & (1 << j)) {
                 putchar('1');
             } else {
