816df5267bc81460c8963580a04fec2b7c3d82be
Gaspar Morales <gasparmorales1@gmail.com>
2023-11-11 06:30:23 -0300

    borrar arreglado

--- free_bits.c
+++ free_bits.c
@@ -2,11 +2,12 @@
 
 void free_bits(unsigned char *bitmap, size_t bitmap_size, size_t units_to_free, size_t unit_index){
     if(unit_index + units_to_free > bitmap_size * 8){
-        return -1;
+        return 0;
     }
 
     for (size_t i = unit_index; i < unit_index + units_to_free && i < bitmap_size * 8; i++) {
-        bitmap[i / 8] &= ~(1 << (i % 8));
+        size_t byte = i / 8;
+        bitmap[byte] &= ~(1 << (7 -(i % 8)));
     }
     printf("Unidades liberadas correctamente.\n");
 }
